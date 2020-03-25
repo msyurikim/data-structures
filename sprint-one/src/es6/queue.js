115381f70928cc48c25e37dffc09aa83fd9ca0f0
Adam Tiller <a.j.tiller@gmail.com>
2020-03-25 15:58:09 -0700

    Add ES6 queue and stack.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.storage[this.size()] = value;
+  }
+
+  dequeue() {
+    var dequeued = this.storage[0];
+    for (var i = 0; i < this.size(); i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[this.size() - 1];
+    return dequeued;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
