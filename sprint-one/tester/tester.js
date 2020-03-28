1b4e0ea2edbdb08e716e9f541c0715e10da85d9e
Adam Tiller <a.j.tiller@gmail.com>
2020-03-28 09:51:49 -0700

    Added options to tester to choose arraylike or fast versions.

--- sprint-one/tester/tester.js
+++ sprint-one/tester/tester.js
@@ -6,7 +6,7 @@ var runTests = function(depth, iterations, type, implementation) {
 
 
   var makeStack = function(implementation) {
-    if (implementation === 'es6') {
+    if (implementation === 'es6-array') {
       class Stack {
         constructor() {
           this.storage = {};
@@ -30,7 +30,37 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = new Stack();
       return tester;
 
-    } else if (implementation === 'functional') {
+    } else if (implementation === 'es6-fast') {
+      class Stack {
+        constructor() {
+          this.storage = {};
+          this.top = 0;
+        }
+
+        push(value) {
+          this.storage[this.size()] = value;
+          this.top++;
+        }
+
+        pop() {
+          if (this.top === 0) {
+            return;
+          }
+          var popped = this.storage[this.size() - 1];
+          delete this.storage[this.size() - 1];
+          this.top--;
+          return popped;
+        }
+
+        size() {
+          return this.top;
+        }
+      }
+
+      var tester = new Stack();
+      return tester;
+
+    } else if (implementation === 'functional-array') {
       var Stack = function() {
         var someInstance = {};
 
@@ -56,7 +86,39 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Stack();
       return tester;
 
-    } else if (implementation === 'functional-shared') {
+    } else if (implementation === 'functional-fast') {
+      var Stack = function() {
+        var someInstance = {};
+
+        var storage = {};
+        var top = 0;
+
+        someInstance.push = function(value) {
+          storage[someInstance.size()] = value;
+          top++;
+        };
+
+        someInstance.pop = function() {
+          if (top === 0) {
+            return;
+          }
+          var result = storage[someInstance.size() - 1];
+          delete storage[someInstance.size() - 1];
+          top--;
+          return result;
+        };
+
+        someInstance.size = function() {
+          return top;
+        };
+
+        return someInstance;
+      };
+
+      var tester = Stack();
+      return tester;
+
+    } else if (implementation === 'functional-shared-array') {
       var Stack = function() {
         var someInstance = {};
         someInstance.stack = {};
@@ -81,7 +143,38 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Stack();
       return tester;
 
-    } else if (implementation === 'prototypal') {
+    } else if (implementation === 'functional-shared-fast') {
+      var Stack = function() {
+        var someInstance = {};
+        someInstance.stack = {};
+        someInstance.top = 0;
+        _.extend(someInstance, stackMethods);
+        return someInstance;
+      };
+
+      var stackMethods = {
+        push: function(value) {
+          this.stack[this.size()] = value;
+          this.top++;
+        },
+        pop: function() {
+          if (this.top === 0) {
+            return;
+          }
+          let popped = this.stack[this.size() - 1];
+          delete this.stack[this.size() - 1];
+          this.top--;
+          return popped;
+        },
+        size: function() {
+          return this.top;
+        }
+      };
+
+      var tester = Stack();
+      return tester;
+
+    } else if (implementation === 'prototypal-array') {
       var Stack = function() {
         var obj = Object.create(stackMethods);
         obj.storage = {};
@@ -105,7 +198,37 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Stack();
       return tester;
 
-    } else if (implementation === 'pseudoclassical') {
+    } else if (implementation === 'prototypal-fast') {
+      var Stack = function() {
+        var obj = Object.create(stackMethods);
+        obj.storage = {};
+        obj.top = 0;
+        return obj;
+      };
+
+      var stackMethods = {
+        push: function(value) {
+          this.storage[this.size()] = value;
+          this.top++;
+        },
+        pop: function() {
+          if (this.top === 0) {
+            return;
+          }
+          var popped = this.storage[this.size() - 1];
+          delete this.storage[this.size() - 1];
+          this.top--;
+          return popped;
+        },
+        size: function() {
+          return this.top;
+        }
+      };
+
+      var tester = Stack();
+      return tester;
+
+    } else if (implementation === 'pseudoclassical-array') {
       var Stack = function() {
         this.storage = {};
       };
@@ -127,7 +250,7 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = new Stack();
       return tester;
 
-    } else if (implementation === 'fastpseudo') {
+    } else if (implementation === 'pseudoclassical-fast') {
       var Stack = function() {
         this.storage = {};
         this.top = 0;
@@ -139,6 +262,9 @@ var runTests = function(depth, iterations, type, implementation) {
       };
 
       Stack.prototype.pop = function() {
+        if (this.top === 0) {
+          return;
+        }
         var popped = this.storage[this.size() - 1];
         delete this.storage[this.size() - 1];
         this.top--;
@@ -156,7 +282,7 @@ var runTests = function(depth, iterations, type, implementation) {
   };
 
   var makeQueue = function(implementation) {
-    if (implementation === 'es6') {
+    if (implementation === 'es6-array') {
       class Queue {
         constructor() {
           this.storage = {};
@@ -184,7 +310,39 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = new Queue();
       return tester;
 
-    } else if (implementation === 'functional') {
+    } else if (implementation === 'es6-fast') {
+      class Queue {
+        constructor() {
+          this.storage = {};
+          this.head = 0;
+          this.tail = -1;
+        }
+
+        enqueue(value) {
+          this.tail++;
+          this.storage[this.tail] = value;
+        }
+
+        dequeue() {
+          var dequeued = this.storage[head];
+          delete this.storage[this.head];
+          this.head++;
+          if (this.size === 0) {
+            this.head = 0;
+            this.tail = -1;
+          }
+          return dequeued;
+        }
+
+        size() {
+          return tail - head + 1;
+        }
+      }
+
+      var tester = new Queue();
+      return tester;
+
+    } else if (implementation === 'functional-array') {
       var Queue = function() {
         var someInstance = {};
         var storage = {};
@@ -212,7 +370,40 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Queue();
       return tester;
 
-    } else if (implementation === 'functional-shared') {
+    } else if (implementation === 'functional-fast') {
+      var Queue = function() {
+        var someInstance = {};
+        var storage = {};
+        var head = 0;
+        var tail = -1;
+
+        someInstance.enqueue = function(value) {
+          tail++;
+          storage[tail] = value;
+        };
+
+        someInstance.dequeue = function() {
+          let removed = storage[head];
+          delete storage[head];
+          head++;
+          if (someInstance.size() === 0) {
+            head = 0;
+            tail = -1;
+          }
+          return removed;
+        };
+
+        someInstance.size = function() {
+          return tail - head + 1;
+        };
+
+        return someInstance;
+      };
+
+      var tester = Queue();
+      return tester;
+
+    } else if (implementation === 'functional-shared-array') {
       var Queue = function() {
         var someInstance = {};
         someInstance.queue = {};
@@ -240,7 +431,40 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Queue();
       return tester;
 
-    } else if (implementation === 'prototypal') {
+    } else if (implementation === 'functional-shared-fast') {
+      var Queue = function() {
+        var someInstance = {};
+        someInstance.queue = {};
+        someInstance.head = 0;
+        someInstance.tail = -1;
+        _.extend(someInstance, queueMethods);
+        return someInstance;
+      };
+
+      var queueMethods = {
+        enqueue: function(value) {
+          this.tail++;
+          this.queue[this.tail] = value;
+        },
+        dequeue: function() {
+          var dequeued = this.queue[head];
+          delete this.queue[head];
+          this.head++;
+          if (this.size === 0) {
+            this.head = 0;
+            this.tail = -1;
+          }
+          return dequeued;
+        },
+        size: function() {
+          return this.tail - this.head + 1;
+        }
+      };
+
+      var tester = Queue();
+      return tester;
+
+    } else if (implementation === 'prototypal-array') {
       var Queue = function() {
         var obj = Object.create(queueMethods);
         obj.storage = {};
@@ -267,7 +491,39 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = Queue();
       return tester;
 
-    } else if (implementation === 'pseudoclassical') {
+    } else if (implementation === 'prototypal-fast') {
+      var Queue = function() {
+        var obj = Object.create(queueMethods);
+        obj.storage = {};
+        obj.head = 0;
+        obj.tail = -1;
+        return obj;
+      };
+
+      var queueMethods = {
+        enqueue: function (value) {
+          this.tail++;
+          this.storage[this.tail] = value;
+        },
+        dequeue: function() {
+          var dequeued = this.queue[head];
+          delete this.storage[head];
+          this.head++;
+          if (this.size === 0) {
+            this.head = 0;
+            this.tail = -1;
+          }
+          return dequeued;
+        },
+        size: function() {
+          return this.tail - this.head + 1;
+        }
+      };
+
+      var tester = Queue();
+      return tester;
+
+    } else if (implementation === 'pseudoclassical-array') {
       var Queue = function() {
         this.storage = {};
       };
@@ -292,7 +548,7 @@ var runTests = function(depth, iterations, type, implementation) {
       var tester = new Queue();
       return tester;
 
-    } else if (implementation === 'fastpseudo') {
+    } else if (implementation === 'pseudoclassical-fast') {
       var Queue = function() {
         this.storage = {};
         this.head = 0;
@@ -308,7 +564,7 @@ var runTests = function(depth, iterations, type, implementation) {
         var dequeued = this.storage[head];
         delete this.storage[this.head];
         this.head++;
-        if (this.size === -1) {
+        if (this.size === 0) {
           this.head = 0;
           this.tail = -1;
         }
@@ -316,7 +572,7 @@ var runTests = function(depth, iterations, type, implementation) {
       };
 
       Queue.prototype.size = function() {
-        return tail - head;
+        return tail - head + 1;
       };
 
       var tester = new Queue();
